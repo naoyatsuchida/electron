aa74decdbf185f615cc2ceb3995c372c507d63c2
naoyatsuchida <naoyatsuchida9@gmail.com>
2022-10-02 14:50:58 +0900

    first

---
+++ electron_tytorial/main.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow, ipcMain } = require("electron");
+const path = require("path");
+
+function handleSetTitle(event, title) {
+  const webContents = event.sender;
+  const win = BrowserWindow.fromWebContents(webContents);
+  win.setTitle(title);
+}
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+  ipcMain.handle("ping", () => "pong");
+  win.loadFile("index.html");
+};
+
+app.whenReady().then(() => {
+  ipcMain.on("set-title", handleSetTitle);
+  createWindow();
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
---
+++ electron_tytorial/preload.js
@@ -0,0 +1,24 @@
+const { contextBridge, ipcRenderer } = require("electron");
+
+contextBridge.exposeInMainWorld("electronAPI", {
+  setTitle: (title) => ipcRenderer.send("set-title", title),
+});
+
+window.addEventListener("DOMContentLoaded", () => {
+  const replaceText = (selector, text) => {
+    const element = document.getElementById(selector);
+    if (element) element.innerText = text;
+  };
+
+  for (const dependency of ["chrome", "node", "electron"]) {
+    replaceText(`${dependency}-version`, process.versions[dependency]);
+  }
+});
+
+contextBridge.exposeInMainWorld("versions", {
+  node: () => process.versions.node,
+  chrome: () => process.versions.chrome,
+  electron: () => process.versions.electron,
+  ping: () => ipcRenderer.invoke("ping"),
+  // 関数だけでなく、変数も公開できます
+});
